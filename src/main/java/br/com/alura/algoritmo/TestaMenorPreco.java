39ec2c449b33c9efedfd51d35a805a58117c5100
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-05-25 18:00:04 -0300

    Aula 6 - Ordenação

--- src/main/java/br/com/alura/algoritmo/TestaMenorPreco.java
+++ src/main/java/br/com/alura/algoritmo/TestaMenorPreco.java
@@ -13,5 +13,23 @@ public class TestaMenorPreco {
                 new Produto("New Civic", 35000)
         };
 
+        int maisBarato = buscaMenor(produtos, 0, 4);
+        System.out.println("O carro " + produtos[maisBarato].getNome()
+                                      + " é o mais barato, e custa R$ "
+                                      + produtos[maisBarato].getPreco());
+    }
+
+    private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
+
+        int maisBarato = inicio;
+
+        for (int atual = inicio; atual <= termino; atual++) {
+            if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+                maisBarato = atual;
+            }
+        }
+
+        return maisBarato;
+
     }
 }
--- src/main/java/br/com/alura/algoritmo/TestaOrdenacao.java
+++ src/main/java/br/com/alura/algoritmo/TestaOrdenacao.java
@@ -9,30 +9,65 @@ public class TestaOrdenacao {
                 new Produto("Lamborghini", 1000000),
                 new Produto("Jipe", 46000),
                 new Produto("Brasilia", 16000),
-                new Produto("Cobalt", 47000),
+                new Produto("Cobalt", 57000),
                 new Produto("New Civic", 35000)
         };
-        
-        for (int atual = 0; atual < produtos.length; atual++) {
 
-            int menor = buscaMenor(produtos, atual, produtos.length - 1);
+        //selectionSort(produtos, produtos.length);
+        novoSort(produtos, produtos.length);
+        imprime(produtos);
 
-            Produto produtoAtual = produtos[atual];
-            Produto produtoMenor = produtos[menor];
+    }
 
-            produtos[atual] = produtoMenor;
-            produtos[menor] = produtoAtual;
+        private static  void imprime(Produto[] produtos){
+            for (Produto produto : produtos) {
+                System.out.println(produto.getNome() + " custa R$ " + produto.getPreco());
+            }
         }
 
-        for (Produto produto : produtos) {
-            System.out.println(produto.getNome() + " custa " + produto.getPreco());
+
+    private static void novoSort(Produto[] produtos, int qtdDeElementos){
+        for (int atual = 1; atual < qtdDeElementos ; atual++) {
+
+            System.out.println("Estou na casa " +atual);
+
+            int analise = atual;
+            while (analise > 0 && produtos[analise].getPreco() < produtos[analise -1].getPreco()) {
+                troca(produtos, analise, analise -1);
+                analise--;
+            }
+
         }
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+    }
+
+    private static void troca(Produto[] produtos, int primeiro, int segundo) {
+        System.out.println("Trocando " + primeiro + " com o " + segundo);
+
+        Produto primeiroProduto = produtos[primeiro];
+        Produto segundoProduto = produtos[segundo];
+        System.out.println("Estou trocando " + primeiroProduto.getNome() + " por " + segundoProduto.getNome());
+
+        produtos[primeiro] = segundoProduto;
+        produtos[segundo] = primeiroProduto;
+    }
+
+    private static void selectionSort(Produto[] produtos, int qtdDeElementos){
+        for (int atual = 0; atual < qtdDeElementos -1; atual++) {
+            System.out.println("Estou na casa " + atual);
+
+            int menor = buscaMenor(produtos, atual, qtdDeElementos - 1);
+            troca(produtos, atual, menor);
+        }
+        System.out.println("----------------------");
     }
 
     private static int buscaMenor(Produto[] produtos, int inicio, int termino) {
 
         int maisBarato = inicio;
-
         for (int atual = inicio; atual <= termino; atual++) {
             if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
                 maisBarato = atual;
@@ -43,4 +78,5 @@ public class TestaOrdenacao {
 
     }
 }
+
 
