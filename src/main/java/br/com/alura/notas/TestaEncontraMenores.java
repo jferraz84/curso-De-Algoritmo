b949bdc52676dce368d709b37b60ab646fb63f04
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2020-05-26 17:23:30 -0300

    Aula 6 - Colocando um elemento no seu lugar o pivô

--- src/main/java/br/com/alura/notas/TestaEncontraMenores.java
+++ src/main/java/br/com/alura/notas/TestaEncontraMenores.java
@@ -4,22 +4,24 @@ public class TestaEncontraMenores {
 
     public static void main(String[] args) {
 
+        Nota guilherme =  new Nota("guilherme", 7);
         Nota[] notas = {
+
                 new Nota("Andre ", 4),
                 new Nota("Carlos", 8.5),
                 new Nota("Ana", 10),
                 new Nota("Jonas", 3),
                 new Nota("Juliana", 6.7),
-                new Nota("Guilherme", 7),
+                guilherme,
                 new Nota("Paulo", 9),
                 new Nota("Mariana", 5),
                 new Nota("Lucia", 9.3)
         };
 
-        int menores = buscaMenores(busca, notas);
-        System.out.println("Numero de noats menores: " + menores);
+        int menores = buscaMenores(guilherme, notas);
+        System.out.println("Numero de notas menores: " + menores);
     }
-    
+
     private static int buscaMenores(Nota busca, Nota[] notas) {
 
         int menores = 0;
@@ -30,4 +32,6 @@ public class TestaEncontraMenores {
         }
         return menores;
     }
+
+
 }
---
+++ src/main/java/br/com/alura/notas/TestaPivota.java
@@ -0,0 +1,46 @@
+package br.com.alura.notas;
+
+public class TestaPivota {
+    public static void main(String[] args) {
+
+        Nota[] notas = {
+                new Nota("Andre ", 4),
+                new Nota("Carlos", 8.5),
+                new Nota("Ana", 10),
+                new Nota("Jonas", 3),
+                new Nota("Juliana", 6.7),
+                new Nota("Lucia", 9.3),
+                new Nota("Paulo", 9),
+                new Nota("Mariana", 5),
+                new Nota("Guilherme", 7)
+        };
+
+        quebraNoPivo(notas, 0, notas.length );
+
+        for (int atual = 0; atual < notas.length; atual++){
+            Nota nota = notas[atual];
+            System.out.println(nota.getAluno() + " = " + nota.getValor());
+        }
+
+    }
+    private static void quebraNoPivo(Nota[] notas, int inicio, int termino) {
+
+        Nota pivo = notas[termino - 1];
+        int menores = 0;
+        for(int analise = 0; analise < termino - 1; analise++) {
+            Nota atual = notas[analise];
+            if(atual.getValor() <= pivo.getValor()) {
+                troca(notas, analise, menores);
+                menores++;
+            }
+        }
+        troca(notas, termino - 1, menores);
+    }
+
+    private static void troca(Nota[] notas, int de, int para) {
+        Nota nota1 = notas[de];
+        Nota nota2 = notas[para];
+        notas[para] = nota1;
+        notas[de] = nota2;
+    }
+}
